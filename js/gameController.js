1794b6fca70975a3ab53aa8020d95b0ac3c8e463
Gary Siu <Gary@untypeable.com>
2015-10-13 02:23:39 +0100

    Each cell is now generated from an array. Each cell has an id based on its position

--- js/gameController.js
+++ js/gameController.js
@@ -3,4 +3,15 @@ angular.module('tttGame')
 
 function GameController(){
   this.title = "Tic-Tac-Toe in Angular!"
+  // this.width = 3;
+  // this.height = 3;
+  this.board = ['0','1','2','3','4','5','6','7','8'];
 }
+
+// function generateBoard(width, height){
+//   var board = []
+//   for(i = 0; i < width * height; i++){
+//     board.push([])
+//   }
+//   return board;
+// }
